8ba64e7bf72705f432de47bd3e253ce2934222c2
OKAN AKSU <20212013087@std.nisantasi.edu.tr>
2024-02-13 20:50:41 +0300

    .

---
+++ DevFolio/Controllers/SkillController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFolio.Models;
+
+namespace DevFolio.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbDevFolioEntities db = new DbDevFolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblSkill.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateSkill(TblSkill p)
+        {
+            db.TblSkill.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteSkill(int id)
+        {
+            var values = db.TblSkill.Find(id);
+            db.TblSkill.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = db.TblSkill.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateSkill(TblSkill p)
+        {
+            var value = db.TblSkill.Find(p.SkillID);
+            value.SkillTitle = p.SkillTitle;
+            value.SkillValue = p.SkillValue;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
